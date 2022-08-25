1c399021a9a6e728381367380b03d1b3074eb5a3
pantera2580 <emmnlcruz@gmail.com>
2022-08-25 20:45:19 -0300

    added weather api in main.js

--- main.js
+++ main.js
@@ -35,4 +35,26 @@ function onClick (event){
   let boton = document.getElementById("enviar");
   boton.addEventListener("click", onClick);
 
-  
+/* ---- APP DEL CLIMA --- */
+
+let ciudad = document.querySelector('.ciudad');
+let temperatura = document.querySelector('.temperatura');
+let tiempo = document.querySelector('.tiempo');
+
+function letWeather(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=-24.18328&lon=-65.33069&appid=a2a967f20fda739b027a2e0aa283722b&lang=es&units=metric')
+        .then(response => response.json())
+        .then(response => {
+            let city = response['name'];
+            let temp = response['main']['temp'];
+            let time = response['weather'][0]['description'];
+
+            ciudad.innerHTML = city;
+            temperatura.innerHTML = temp;
+            tiempo.innerHTML = time;
+        })
+
+        .catch(err => console.error(err));
+
+}
+letWeather();
